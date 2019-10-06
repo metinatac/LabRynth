6dd31fa288db63dc8f8d5216f7b83af6e345cb6e
Metin Atac <metin_a74@hotmail.de>
2019-10-06 15:31:04 +0200

    PlayerControlls

--- Develop/Assets/Scripts/PlayerController.cs
+++ Develop/Assets/Scripts/PlayerController.cs
@@ -10,49 +10,82 @@ public class PlayerController : MonoBehaviour
     Coordinates co;
     bool startPressed;
 
+    public float turnSpeed;
+
+    public float walkSpeed;
+
+    public float camHight;
+
+    public float camDistance;
+
+    [SerializeField]
+    private Joystick stickTurn;
+    [SerializeField]
+    private GameObject WalkStickOb;
+
+    [SerializeField]
+    private Joystick stickWalk;
+
+    [SerializeField]
+    private GameObject PlayerCam;
+
+    private Rigidbody rb;
     bool movePlayer;
     bool triggerIsOk;
+
+
+    private Vector3 forward;
+    private Vector3 right;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        forward = this.gameObject.transform.forward;
+        right = this.gameObject.transform.right;
+
+
         co = Coordinates.Instance;
         startPressed = false;
         movePlayer = false;
         triggerIsOk = true;
-
+        PlayerCam.transform.rotation = this.gameObject.transform.rotation;
+   rb = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+  
+
+     void FixedUpdate()
     {
-
-        if (movePlayer)
+        if (triggerIsOk)
         {
-            if (triggerIsOk)
-            {
-                float moveSpeed = 2.0f;
-                this.gameObject.transform.position += this.gameObject.transform.forward * Time.deltaTime * moveSpeed;
-            }
-          
-        }
+            rb.velocity = PlayerCam.transform.forward * (stickWalk.Vertical * walkSpeed) * Time.deltaTime +
+            PlayerCam.transform.right * (stickWalk.Horizontal * walkSpeed) * Time.deltaTime;
 
-
-
-        if (Input.touchCount == 1)
-        {
-            if (!IsPointerOverUIObject())
-            {
-                float rotateSpeed = 0.2f;
-                Touch touchZero = Input.GetTouch(0);
-
-                //Rotate the model based on offset
-                Vector3 localAngle = this.gameObject.transform.localEulerAngles;
-                localAngle.y -= rotateSpeed * touchZero.deltaPosition.x;
-
-                this.gameObject.transform.localEulerAngles = localAngle;
-            }
 
         }
+        float Xpos = this.gameObject.transform.position.x;
+        float Ypos = this.gameObject.transform.position.y + camHight;
+        float Zpos = this.gameObject.transform.position.z;
+
+        PlayerCam.transform.position = new Vector3(Xpos, Ypos, Zpos);
+
+        // turning with Joystick around Y-Axis.
+        Vector3 localEuler = this.gameObject.transform.localEulerAngles;
+        Vector3 camEuler = PlayerCam.transform.localEulerAngles;
+        Vector3 walkEuler = WalkStickOb.transform.localEulerAngles;
+
+
+
+
+        walkEuler.z = stickTurn.Horizontal * turnSpeed;
+        localEuler.y += stickTurn.Horizontal * turnSpeed;
+        camEuler.y += stickTurn.Horizontal * turnSpeed;
+        WalkStickOb.transform.localEulerAngles = walkEuler;
+        PlayerCam.transform.localEulerAngles = camEuler;
+        this.gameObject.transform.localEulerAngles = localEuler;
+
     }
 
     public void StartClicked()
